a249d34b0d060c07ae7e34e8da133eee4e3e0d8f
Nick Sinclair <nick.sinclair@itential.com>
2021-02-20 15:12:41 -0500

    Refactored sketch.js based on shape-grid, added UI elements and styles, fixed recording problems

--- Form.js
+++ Form.js
@@ -1,11 +1,10 @@
-// colorMode(HSB);
-
 class Form {
   constructor(posX, posY) {
     this.x = posX;
     this.y = posY;
     this.form = floor(random(3)) + 1;
-    this.materialColor = getRandomFromPalette();
+    this.paletteIndex = floor(random(0, PALETTE.length - 1));
+    this.materialColor = PALETTE[this.paletteIndex];
     this.formSize = FORM_SIZE;
     this.transform = floor(random(4));
     this.inc = floor(random(3)) + 1;
@@ -13,11 +12,14 @@ class Form {
   }
 
   render() {
-    // translate(this.x, this.y);
-    
     noStroke();
     specularMaterial(this.materialColor);
 
     renderForm(this.form, this.formSize);
+  }
+
+  update() {
+    this.formSize = FORM_SIZE;
+    this.materialColor = PALETTE[this.paletteIndex];
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,123 +1,154 @@
+p5.disableFriendlyErrors = true;
+
+let ROWS = 10;
+let COLS = 10;
+let FORM_SIZE = 40;
+
+let prev_rows = ROWS;
+let prev_cols = COLS;
+// let prevFormSize = FORM_SIZE;
+
+// UI DOM ELEMENTS
+let primaryColorPicker;
+let secondaryColorPicker;
+let backgroundColorPicker;
+
+let rowSlider;
+let columnSlider;
+// let formSizeSlider;
+
 // VIDEO EXPORT
 const fps = 30;
 let frame = 1;
-let capturer;
-let btn;
+let capturer = new CCapture({
+  format: "png",
+  framerate: fps,
+});
+let recording = false;
+let recordButton;
 
-function record() {
-  capturer = new CCapture({
-    format: 'png',
-    framerate: fps
-  });
-  
-  capturer.start();
-  
-  btn.textContent = "stop recording";
-  
-  btn.onclick = e => {
-    capturer.stop();
-    capturer.save();
-    capturer = null;
-    
-    btn.textContent = "start recording";
-    btn.onclick = record;
-  };
-}
-
-let FORM_SIZE = 60;
-
-// COLORS
-let theme1 = [];
-let theme2 = [];
-let theme3 = [];
-let theme4 = [];
-
+// THEMES
+let dynamicTheme = [];
+// let theme1 = [];
+// let theme2 = [];
+// let theme3 = [];
+// let theme4 = [];
 let PALETTE = [];
 
 // LAYOUT
-const ROWS = 30;
-const COLS = 30;
+let MARGIN = FORM_SIZE * 4;
+let PADDING = FORM_SIZE * 0.5;
 
-const MARGIN = FORM_SIZE * 4;
-const PADDING = FORM_SIZE * 0.5;
+let GRIDBOX = FORM_SIZE + PADDING;
+let START = FORM_SIZE + MARGIN;
+let ROW_START = -((GRIDBOX * ROWS) / 2) + FORM_SIZE * 0.75;
+let COL_START = -((GRIDBOX * COLS) / 2) + FORM_SIZE * 0.75;
 
-const GRIDBOX = FORM_SIZE + PADDING;
-const START = FORM_SIZE + MARGIN;
-const START2 = -(GRIDBOX * COLS / 2) + FORM_SIZE;
+let totalX = START + MARGIN + GRIDBOX * COLS;
+let totalY = START + MARGIN + GRIDBOX * ROWS;
 
 // FORMS
 const forms = [];
 let angle = 0;
 
 function setup() {
-  const totalX = START + MARGIN + GRIDBOX * COLS;
-  const totalY = START + MARGIN + GRIDBOX * ROWS;
-  console.log(totalX);
+  pixelDensity(1);
+  colorMode(HSB);
+
+  let maxRows = ceil((windowHeight - (START + MARGIN)) / GRIDBOX);
+  let maxCols = floor((windowWidth - (START + MARGIN + 350)) / GRIDBOX);
+
+  // UI DOM ELEMENTS
+  // parent ui element
+  const ui = createDiv().class("control-panel");
+
+  // labels
+  const primaryLabel = createDiv("PRIMARY COLOR").class("label").parent(ui);
+  const secondaryLabel = createDiv("SECONDARY COLOR").class("label").parent(ui);
+  const backgroundLabel = createDiv("BACKGROUND COLOR")
+    .class("label")
+    .parent(ui);
+
+  const rowLabel = createDiv("ROWS").class("label").parent(ui);
+  const colLabel = createDiv("COLUMNS").class("label").parent(ui);
+
+  // color pickers
+  primaryColorPicker = createColorPicker(color(180, 100, 30))
+    .class("color-picker")
+    .parent(primaryLabel);
+  secondaryColorPicker = createColorPicker(color(180, 100, 60))
+    .class("color-picker")
+    .parent(secondaryLabel);
+  backgroundColorPicker = createColorPicker(color(180, 100, 90))
+    .class("color-picker")
+    .parent(backgroundLabel);
+
+  // sliders
+  rowSlider = createSlider(2, maxRows, 10, 1).class("slider").parent(rowLabel);
+  colSlider = createSlider(2, maxCols, 10, 1).class("slider").parent(colLabel);
+  // formSizeSlider = createSlider(10, 30, 10, 1).class("slider").parent(ui);
+
+  // VIDEO EXPORT
+  // frameRate(fps);
+  recordButton = document.createElement("button");
+  recordButton.textContent = "START RECORDING";
+  document.getElementsByClassName("control-panel")[0].appendChild(recordButton);
+  recordButton.onclick = record;
+  // recordButton.click(); // starts recording immediately
+
+  // LAYOUT
   createCanvas(totalX, totalY, WEBGL);
 
   // MODES
   rectMode(CENTER);
   angleMode(DEGREES);
-  colorMode(HSB);
 
   // COLOR
-  theme1 = [
-    color(0, 0, 10), // dark gray
-    color(0, 0, 40), // light gray
-    color(0, 0, 70) // very light gray
-  ];
+  // theme1 = [
+  //   color(0, 0, 10), // dark gray
+  //   color(0, 0, 40), // light gray
+  //   color(0, 0, 70), // very light gray
+  // ];
 
-  theme2 = [
-    color(180, 100, 30), // dark cyan
-    color(180, 100, 60), // light cyan
-    color(180, 100, 90) // very light cyan
-  ];
+  // theme2 = [
+  //   color(180, 100, 30), // dark cyan
+  //   color(180, 100, 60), // light cyan
+  //   color(180, 100, 90), // very light cyan
+  // ];
 
-  theme3 = [
-    color(300, 100, 30), // dark magenta
-    color(300, 100, 60), // light magenta
-    color(300, 100, 90) // very light magenta
-  ];
-  
-  theme4 = [
-    color(60, 100, 30), // dark yellow
-    color(60, 100, 60), // light yellow
-    color(60, 100, 90) // very light yellow
-  ];
+  // theme3 = [
+  //   color(300, 100, 30), // dark magenta
+  //   color(300, 100, 60), // light magenta
+  //   color(300, 100, 90), // very light magenta
+  // ];
 
-  PALETTE = theme3;
+  // theme4 = [
+  //   color(60, 100, 30), // dark yellow
+  //   color(60, 100, 60), // light yellow
+  //   color(60, 100, 90), // very light yellow
+  // ];
+  calculatePalette();
 
   // FORMS
-  for (let x = 0; x < ROWS; x++) {
-    for (let y = 0; y < COLS; y++) {
-      const posX = START2 + (x * GRIDBOX);
-      const posY = START2 + (y * GRIDBOX);
-
-      forms.push(new Form(posX, posY))
-    }
-  }
-  
-  // VIDEO EXPORT
-  frameRate(fps);
-  btn = document.createElement("button");
-  btn.textContent = "start recording";
-  document.body.appendChild(btn);
-  btn.onclick = record;
-  // btn.click(); // starts recording immediately
+  generateForms();
 }
 
 function draw() {
-  let dx = sin(angle) * width / 2;
-  let dy = cos(angle) * height / 2;
+  let dx = (sin(angle) * width) / 2;
+  // let dy = (cos(angle) * height) / 2;
 
-  let v = createVector(dx, dy, 0);
-  v.normalize();
+  // let v = createVector(dx, dy, 0);
+  // v.normalize();
+
+  // LAYOUT
+  calculateLayout();
+
+  // COLOR
+  calculatePalette();
 
   // LIGHTS
   ambientLight(120);
   pointLight(0, 0, 100, dx, 0, 0);
-  // pointLight(getRandomFromPalette(), 0, dy, 0);
-  // directionalLight(0, 0, 0, v);
 
   angle += 0.001;
 
@@ -125,33 +156,108 @@ function draw() {
   noStroke();
 
   // FORM GRID
-  for (let i = 0; i < forms.length; i++) {
-    const currForm = forms[i];
-    push();
-    translate(currForm.x, currForm.y);
-    applyTransformation(currForm.transform, currForm.counter);
-    currForm.render();
-    currForm.counter += currForm.inc;
-    pop();
-  }
+  renderForms();
 
   // BORDER
   push();
   noFill();
   stroke(PALETTE[0]);
-  rect(0, 0, START + (GRIDBOX * COLS), START + (GRIDBOX * ROWS))
+  rect(0, 0, START + GRIDBOX * COLS, START + GRIDBOX * ROWS);
   pop();
 
+  // console.log(frameRate());
+
+  // Uncomment to stop interactivity
   // noLoop();
-  
-  // console.log(frame++);
-  
+
   // Trigger video capture
-  if (capturer) {
+  if (recording) {
     capturer.capture(document.getElementById("defaultCanvas0"));
   }
 }
 
+function generateForms() {
+  forms.splice(0, forms.length);
 
+  for (let x = 0; x < COLS; x++) {
+    for (let y = 0; y < ROWS; y++) {
+      const posX = COL_START + x * GRIDBOX;
+      const posY = ROW_START + y * GRIDBOX;
 
+      forms.push(new Form(posX, posY));
+    }
+  }
+}
 
+function renderForms() {
+  forms.forEach((form) => {
+    push();
+    translate(form.x, form.y);
+    applyTransformation(form.transform, form.counter);
+    form.update();
+    form.render();
+    form.counter += form.inc;
+    pop();
+  });
+}
+
+function calculateLayout() {
+  ROWS = rowSlider.value();
+  COLS = colSlider.value();
+
+  MARGIN = FORM_SIZE * 4;
+  PADDING = FORM_SIZE * 0.5;
+
+  GRIDBOX = FORM_SIZE + PADDING;
+  START = FORM_SIZE + MARGIN;
+  COL_START = -((GRIDBOX * COLS) / 2) + FORM_SIZE * 0.75;
+  ROW_START = -((GRIDBOX * ROWS) / 2) + FORM_SIZE * 0.75;
+  // console.log(START2);
+
+  totalX = START + MARGIN + GRIDBOX * COLS;
+  totalY = START + MARGIN + GRIDBOX * ROWS;
+
+  if (evaluateResize()) {
+    generateForms();
+  }
+
+  prev_rows = ROWS;
+  prev_cols = COLS;
+
+  resizeCanvas(totalX, totalY);
+}
+
+function calculatePalette() {
+  dynamicTheme = [
+    primaryColorPicker.color(),
+    secondaryColorPicker.color(),
+    backgroundColorPicker.color(),
+  ];
+
+  PALETTE = dynamicTheme;
+}
+
+function evaluateResize() {
+  if (prev_rows != ROWS) {
+    return true;
+  }
+  if (prev_cols != COLS) {
+    return true;
+  }
+}
+
+function record() {
+  recording = true;
+  capturer.start();
+
+  recordButton.textContent = "STOP RECORDING";
+
+  recordButton.onclick = (e) => {
+    capturer.stop();
+    capturer.save();
+    recording = false;
+
+    recordButton.textContent = "START RECORDING";
+    recordButton.onclick = record;
+  };
+}
